25338b9f6ee05e839812b4d554f4abe90eb52b4c
fabio <fabio@fabio-HP-Pavilion-dv4-Notebook-PC>
2014-08-24 12:21:18 -0500

    purificadores

--- src/Vista/Principal.java
+++ src/Vista/Principal.java
@@ -47,11 +47,9 @@ public class Principal extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(1360, 743));
         setResizable(false);
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Inventory-maintenance-icon.png"))); // NOI18N
@@ -97,12 +95,6 @@ public class Principal extends javax.swing.JFrame {
 
         jMenu1.setForeground(java.awt.Color.white);
         jMenu1.setText("Archivos");
-
-        jMenuItem1.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
-        jMenuItem1.setForeground(java.awt.Color.white);
-        jMenuItem1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/folder-invoices-icon.png"))); // NOI18N
-        jMenuItem1.setText("Factura Existente");
-        jMenu1.add(jMenuItem1);
 
         jMenuItem2.setFont(new java.awt.Font("DejaVu Sans", 1, 12)); // NOI18N
         jMenuItem2.setForeground(java.awt.Color.white);
@@ -181,7 +173,6 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
